b8d6533e14422f2b88ce7556097e4e4386f722c5
raawlinss <revanalicann@gmail.com>
2026-05-07 22:05:57 +0400

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+APP_PORT="${PORT:-7860}"
+
 echo "Starting MediaMTX..."
-mediamtx /app/mediamtx.yml &
+sed "s|http://127.0.0.1:7860/|http://127.0.0.1:${APP_PORT}/|g" /app/mediamtx.yml > /tmp/mediamtx.yml
+mediamtx /tmp/mediamtx.yml &
 MEDIAMTX_PID=$!
 
 # Wait for MediaMTX to be ready
